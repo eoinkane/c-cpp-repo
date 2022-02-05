70535994fdbdd4e72c422853d1c8d7b1c4f8c708
eoinkane <eoinkane23@gmail.com>
2022-02-05 14:28:43 +0000

    Size of primitive variable, int type

--- src/hello.c
+++ src/hello.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world!\n");
+    int integerVariable;
+    size_t size;
+
+    size = sizeof(integerVariable);
+
+    printf("Size of Integer Variable is %zu bytes\n", size);
+
     return 0;
 }
