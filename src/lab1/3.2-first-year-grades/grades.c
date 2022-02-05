3ffaeafaf54574d77a7dbf4269ccf398dc405bc4
eoinkane <eoinkane23@gmail.com>
2022-02-05 15:59:39 +0000

    Initial grades program

--- src/lab1/3.2-first-year-grades/grades.c
+++ src/lab1/3.2-first-year-grades/grades.c
@@ -2,6 +2,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world!\n");
+    printf("Grades program starting\n");
+
+    float firstYearGradesArray[6];
+
+    printf("Adding grades to the array\n");
+
+    firstYearGradesArray[0] = 76;
+    firstYearGradesArray[1] = 69.6;
+
+    printf("Trimester 1 Grades %f, %f", firstYearGradesArray[0], firstYearGradesArray[1]);
+
     return 0;
 }
