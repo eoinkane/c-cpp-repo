eeb44a9eca090dd9656ace96b3a8187de4d8c2c2
eoinkane <eoinkane23@gmail.com>
2022-02-09 11:34:40 +0000

    Program that stores grades for my first year modules

--- src/lab1/3.2-first-year-grades/grades.c
+++ src/lab1/3.2-first-year-grades/grades.c
@@ -8,10 +8,15 @@ int main() {
 
     printf("Adding grades to the array\n");
 
-    firstYearGradesArray[0] = 76;
-    firstYearGradesArray[1] = 69.6;
+    firstYearGradesArray[0] = 76; // Software Development 1 (Tri 1)
+    firstYearGradesArray[1] = 69.6; // Information Systems in Organisations (Tri 1)
+    firstYearGradesArray[2] = 100; // Human Computer Interface (Blanket Pass) (Tri 2)
+    firstYearGradesArray[3] = 100; // Mathematics for Software Engineering (Blanket Pass) (Tri 2)
+    firstYearGradesArray[4] = 85; // Computer Systems (Tri 3)
+    firstYearGradesArray[5] = 68.5; // Professional Practice (Tri 3) 
 
-    printf("Trimester 1 Grades %f, %f", firstYearGradesArray[0], firstYearGradesArray[1]);
+    printf("Number of grades %li\n", sizeof(firstYearGradesArray)/sizeof(firstYearGradesArray[0]) );
+    printf("Trimester 1 Grades %f, %f\n", firstYearGradesArray[0], firstYearGradesArray[1]);
 
     return 0;
 }
